aabe1f9ea27a8fe7ee47e7720375cb4ab80a07d1
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-09-05 22:00:27 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,18 +1,17 @@
 // proxy.js
 
-const isFn = sth => typeof sth === 'function';
+const isFn     = sth => typeof sth      === 'function';
+const isSymbol = sth => typeof property === 'symbol';
 
-export function createModuleProxy (modflow) {
-
+function createModuleProxy (modflow) {
   return new Proxy (modflow, {
-
     get (target, property, receiver) {
       // case 1: symbols / inspect / native internals
       // case 2: public Modflow API
       // case 3: unknown module
-      return (typeof property === 'symbol') ? Reflect.get(target, property, receiver)    
-           : (property in target)           ? Reflect.get(target, property, receiver)
-           : !target.has(property)          ? undefined;
+      return (isSymbol(property))  ? Reflect.get(target, property, receiver)    
+           : (property in target)  ? Reflect.get(target, property, receiver)
+           : !target.has(property) ? undefined;
            : createModuleStub(target, property);
     }
   });
@@ -32,7 +31,7 @@ function createModuleStub (modflow, name) {
         return (resolve, reject) => load().then(resolve, reject);
       }
 
-      // useful debugging.
+      // useful debugging
       if (property === 'toString') {
         return () => `[Modflow module: ${name}]`;
       }
@@ -40,40 +39,21 @@ function createModuleStub (modflow, name) {
       // direct properties
       return (...args) =>
         load().then(module => {
-
-          const value =
-            module?.[property] ??
-            module?.default ??
-            module;
-
-          if (typeof value !== 'function') {
-            return value;
-          }
-
-          return value.apply(module, args);
+          const value = module?.[property] ?? module?.default ?? module;
+          return !isFn(value) ? value : value.apply(module, args);
         });
     },
 
-    apply(_, __, args) {
-
-      /*
-       * mod.foo(...)
-       */
+    apply (_, __, args) {
       return load().then(module => {
-
-        const callable =
-          typeof module === 'function'
-            ? module
-            : module?.default;
-
-        if (typeof callable !== 'function') {
-          throw new TypeError(
-            `Module "${name}" is not callable.`
-          );
-        }
-
+        const callable = isFn(module) ? module : module?.default;
+        if (!isFn(callable)) throw new TypeError(`Module "${name}" is not callable.`);      
         return callable(...args);
       });
     }
   });
-          }
+}
+
+// :::::: EXPORT
+
+export { createModuleProxy };
